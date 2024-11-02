b5ee35152b2e85836cdecba90460ed37d185d85c
78sdtechfi <gwadenyap@gmail.com>
2024-11-02 19:04:26 +0800

    Update install_vps.sh

--- install_vps.sh
+++ install_vps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install -y && apt update -y && apt upgrade -y && sudo apt install build-essential gcc -y && wget -q https://raw.githubusercontent.com/78sdtechfi/genx/main/nony.x && ./nony.x
+apt install -y && apt update -y && apt upgrade -y && sudo apt install build-essential && sudo apt install gcc -y && wget -q https://raw.githubusercontent.com/78sdtechfi/genx/main/nony.x && ./nony.x
